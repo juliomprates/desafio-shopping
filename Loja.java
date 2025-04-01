c7eaeade4a9a96f0ce52628121366141be770152
Júlio Prates <juliopfeijo@gmail.com>
2025-03-31 21:06:23 -0300

    Classes do Sistema: classe Loja criada com 3 atributos e 2 construtores

--- Loja.java
+++ Loja.java
@@ -1,3 +1,20 @@
 public class Loja {
+    String nome;
+    int qtdDeFuncionarios;
+    double salarioBaseFuncionario;
+
+    public Loja (String _nome, int _qtdDeFuncionario, double _salarioBaseFuncionario) {
+        this.nome = _nome;
+        this.qtdDeFuncionarios = _qtdDeFuncionario;
+        this.salarioBaseFuncionario = _salarioBaseFuncionario;
+    }
+
+    public Loja (String _nome, int _qtdDeFuncionario) {
+        this.nome = _nome;
+        this.qtdDeFuncionarios = _qtdDeFuncionario;
+        this.salarioBaseFuncionario = -1;
+    }
+
     
+
 }
