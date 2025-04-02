be7e92ad9137b39fa26d21322913f6d77e1532e1
Júlio Prates <juliopfeijo@gmail.com>
2025-04-01 23:20:58 -0300

    Classes do Sistema: criação da classe Data com 3 atributos e 1 construtor

---
+++ Data.java
@@ -0,0 +1,9 @@
+public class Data {
+    private int dia, mes, ano;
+
+    public Data(int _dia, int _mes, int _ano) {
+        this.dia = _dia;
+        this.mes = _mes;
+        this.ano = _ano;
+    }
+}
