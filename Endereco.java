2c64fb31c271b94d40cb41a70e32b80664eefb42
Júlio Prates <juliopfeijo@gmail.com>
2025-04-01 22:45:24 -0300

    Classe Endereco: adição dos métodos getters e setters

--- Endereco.java
+++ Endereco.java
@@ -11,7 +11,65 @@ public class Endereco {
             this.cep = _cep;
             this.numero = _numero;
             this.complemento = _complemento;
+        }
+    
+    // getters
+    public String getNomeDaRua() {
+        return nomeDaRua;
     }
 
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public String getCep() {
+        return cep;
+    }
     
+    public String getNumero() {
+        return numero;
+    }
+    
+    public String getComplemento() {
+        return complemento;
+    }
+    
+    // setters
+    public void setNomeDaRua(String _nomeDaRua) {
+        this.nomeDaRua = _nomeDaRua;
+    }
+
+    public void setCidade(String _cidade) {
+        this.cidade = _cidade;
+    }
+
+    public void setEstado(String _estado) {
+        this.estado = _estado;
+    }
+
+    public void setPais(String _pais) {
+        this.pais = _pais;
+    }
+
+    public void setCep(String _cep) {
+        this.cep = _cep;
+    }
+
+    public void setNumero(String _numero) {
+        this.numero = _numero;
+    }
+
+    public void setComplemento(String _complemento) {
+        this.complemento = _complemento;
+    }
+
+
 }
