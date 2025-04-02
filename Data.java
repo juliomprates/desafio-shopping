2dded581cf18762df18b648dc546e580e7213630
Júlio Prates <juliopfeijo@gmail.com>
2025-04-02 02:07:55 -0300

    Melhoria - Classe Data: adiciona validação aos parâmetros passados no construtor da classe, definindo um padrão que condiz com a realidade

--- Data.java
+++ Data.java
@@ -2,9 +2,90 @@ public class Data {
     private int dia, mes, ano;
 
     public Data(int _dia, int _mes, int _ano) {
-        this.dia = _dia;
-        this.mes = _mes;
-        this.ano = _ano;
+       /* VALIDAÇÃO DOS PARÂMENTROS */
+        
+        // checa se o valor de _dia é valido
+        // se não for, atribui uma data padrão
+        boolean diaValido = false;
+        if (_dia >= 1 && _dia <= 31) {
+            diaValido = true;
+        } else {
+            /*Data Padrão */
+            this.dia = 1;
+            this.mes = 1;
+            this.ano = 2000;
+            System.err.println("Erro: A data inserida é inválida");
+            return;
+        }
+        
+        // checa se o valor de _mes é válido
+        // se não for, atribui uma data padrão
+        boolean mesValido = false;
+        if (_mes >= 1 && _mes <= 12) {
+            mesValido = true;
+        } else {
+            /*Data Padrão */
+            this.dia = 1;
+            this.mes = 1;
+            this.ano = 2000;
+            System.err.println("Erro: A data inserida é inválida");
+            return;
+        }
+
+        // checa se o ano é bissexto
+        boolean ehBissexto = false;
+        if ((_ano % 4 == 0 && _ano % 100 != 0) || (_ano % 400 == 0)) {
+            ehBissexto = true;
+        } 
+
+        // define que o dia 29 só pode ser atribuído ao mês 2 em anos bissextos
+        if (diaValido && mesValido) {
+            if(( _dia == 29) && (_mes == 2 && ehBissexto)){
+                this.dia = _dia;
+                this.mes = _mes;
+                this.ano = _ano;
+                return;
+            }
+        }
+
+        // define que o mês 2 só pode receber até o dia 28 em anos não bissextos
+        if (diaValido && mesValido) {
+            if (_mes == 2) {
+                if (_dia > 28) {
+                    /* Data Padrão */
+                    this.dia = 1;
+                    this.mes = 1;
+                    this.ano = 2000;
+                    System.err.println("Erro: A data inserida é inválida");
+                    return;
+                }
+            }
+        }
+
+        // verifica se o mês pode receber o dia 31
+        // se não puder, atribui uma data padrão
+        if (diaValido && mesValido) {  // true true
+            if (_dia == 31) { // true
+                if ((_mes == 2) || (_mes == 4) || (_mes == 6) || (_mes == 9) || (_mes == 11)) { 
+                    /* Data Padrão */
+                    this.dia = 1;
+                    this.mes = 1;
+                    this.ano = 2000;
+                    System.err.println("Erro: A data inserida é inválida");
+                    return;
+                } else {
+                    this.dia = _dia;
+                    this.mes = _mes;
+                    this.ano = _ano;
+                }
+            } else {
+                this.dia = _dia;
+                this.mes = _mes;
+                this.ano = _ano;
+            }
+
+        }
+        
     }
 
     // getters
