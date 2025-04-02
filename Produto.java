a78a84ed6822cba2dfc1d978560901eea5ad1954
Júlio Prates <juliopfeijo@gmail.com>
2025-04-01 22:23:46 -0300

    Classes do Sistema: classe Produto criada com 2 atributos e 1 construtor

---
+++ Produto.java
@@ -0,0 +1,10 @@
+public class Produto {
+    private String nome;
+    private double preco;
+
+    public Produto(String _nome, double _preco) {
+        this.nome = _nome;
+        this.preco = _preco;
+    }
+
+}
