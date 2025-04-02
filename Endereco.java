91b3d9443d993061175167c5efdafb19d8095a68
Júlio Prates <juliopfeijo@gmail.com>
2025-04-01 22:41:58 -0300

    Classes do Sistema: criação da classe Endereco com 7 atributos e 1 construtor

---
+++ Endereco.java
@@ -0,0 +1,17 @@
+public class Endereco {
+    private String nomeDaRua, cidade, estado, pais, cep, numero, complemento;
+
+    public Endereco(String _nomeDaRua, String _cidade, String _estado, String _pais, 
+                    String _cep, String _numero, String _complemento) {
+
+            this.nomeDaRua = _nomeDaRua;
+            this.cidade = _cidade;
+            this.estado = _estado;
+            this.pais = _pais;
+            this.cep = _cep;
+            this.numero = _numero;
+            this.complemento = _complemento;
+    }
+
+    
+}
