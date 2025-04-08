b3713c5a51f50cc9c4e15a37a33dbf3a88d9702e
Júlio Prates <juliopfeijo@gmail.com>
2025-04-07 21:15:43 -0300

    Classe Data: mudança no método que retorna a data formatada para melhor visualização

--- Data.java
+++ Data.java
@@ -120,13 +120,13 @@ public class Data {
 
     public String formatada(){
         if((dia < 10) && (mes < 10)){
-            return "Data: 0"+dia+"/0"+mes+"/"+ano;
+            return "0"+dia+"/0"+mes+"/"+ano;
         } else if (dia < 10) {
-            return "Data: 0"+dia+"/"+mes+"/"+ano;
+            return "0"+dia+"/"+mes+"/"+ano;
         } else if (mes < 10) {
-            return "Data: "+dia+"/0"+mes+"/"+ano;
+            return dia+"/0"+mes+"/"+ano;
         } else {
-            return "Data: "+dia+"/"+mes+"/"+ano;
+            return dia+"/"+mes+"/"+ano;
         }
     }
 
