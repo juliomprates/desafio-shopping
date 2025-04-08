2957cf8aa1ee1e28c1fe1ffec5e5f535bf59e83f
Júlio Prates <juliopfeijo@gmail.com>
2025-04-07 21:13:13 -0300

    Classe Loja: associação com as classes Endereco e Data por meio de atributos

--- Loja.java
+++ Loja.java
@@ -2,32 +2,47 @@ public class Loja {
     private String nome;
     private int qtdDeFuncionarios;
     private double salarioBaseFuncionario;
+    private Endereco endereco;
+    private Data dataDeFundacao;
 
-    public Loja (String _nome, int _qtdDeFuncionario, double _salarioBaseFuncionario) {
+    public Loja (String _nome, int _qtdDeFuncionario, double _salarioBaseFuncionario, 
+                 Endereco _endereco, Data _dataDeFundacao){
         this.nome = _nome;
         this.qtdDeFuncionarios = _qtdDeFuncionario;
         this.salarioBaseFuncionario = _salarioBaseFuncionario;
+        this.endereco = _endereco;
+        this.dataDeFundacao = _dataDeFundacao;
     }
 
-    public Loja (String _nome, int _qtdDeFuncionario) {
+    public Loja (String _nome, int _qtdDeFuncionario, Endereco _endereco, Data _dataDeFundacao){
         this.nome = _nome;
         this.qtdDeFuncionarios = _qtdDeFuncionario;
         this.salarioBaseFuncionario = -1;
+        this.endereco = _endereco;
+        this.dataDeFundacao = _dataDeFundacao;
     }
 
     // getters
-    public String getNome() {
+    public String getNome(){
         return nome;
     }
     
-    public int getQtdDeFuncionarios() {
+    public int getQtdDeFuncionarios(){
         return qtdDeFuncionarios;
     }
     
-    public double getSalarioBaseFuncionario() {
+    public double getSalarioBaseFuncionario(){
         return salarioBaseFuncionario;
     }
     
+    public Endereco getEndereco(){
+        return endereco;
+    }
+
+    public Data getDataDeFundacao(){
+        return dataDeFundacao;
+    }
+
     // setters
     public void setNome(String _nome) {
         this.nome = _nome;
@@ -41,10 +56,19 @@ public class Loja {
         this.salarioBaseFuncionario = _salarioBaseFuncionario;
     }
 
+    public void setEndereco(Endereco _endereco){
+        this.endereco = _endereco;
+    }
+
+    public void setDataDeFundacao(Data _dataDeFundacao){
+        this.dataDeFundacao = _dataDeFundacao;
+    }
+
     // formatação legível dos atributos da classe
     public String toString (){
         return "Loja { nome: "+nome+", Quantidade de Funcionários: "+qtdDeFuncionarios+
-                ", Salário Base dos Funcionários: "+salarioBaseFuncionario+" }";
+                ", Salário Base dos Funcionários: "+salarioBaseFuncionario+",\n"+
+                endereco.toString()+",\nData de Fundação: "+dataDeFundacao.formatada();
     }
     
 
