95b9af899ba72978ff811c1b1cdf98bcae6b0045
Júlio Prates <juliopfeijo@gmail.com>
2025-04-23 21:14:34 -0300

    feat(classes): adiciona a subclasse Cosmetico, da classe Loja, com o atributo único de taxa de comercialização e a sobrescrita do método toString

---
+++ Cosmetico.java
@@ -0,0 +1,34 @@
+import classesPrincipais.Data;
+import classesPrincipais.Endereco;
+import classesPrincipais.Loja;
+
+public class Cosmetico extends Loja {
+    private double taxaComercializacao;
+
+    public Cosmetico(String _nome, int _quantidadeFuncionario, double _salarioBaseFuncionario,
+                     Endereco _endereco, Data _dataDeFundacao) {
+        super(_nome, _quantidadeFuncionario, _salarioBaseFuncionario, _endereco, _dataDeFundacao);
+        this.setTaxaComercializacao(-1);
+    }
+
+    public Cosmetico(String _nome, int _quantidadeFuncionario, Endereco _endereco, Data _dataDeFundacao){
+        super(_nome, _quantidadeFuncionario, _endereco, _dataDeFundacao);
+        this.setSalarioBaseFuncionario(-1);
+        this.setTaxaComercializacao(-1);
+    }
+
+    public double getTaxaComercializacao() {
+        return taxaComercializacao;
+    }
+
+    public void setTaxaComercializacao(double taxaComercializacao) {
+        this.taxaComercializacao = taxaComercializacao;
+    }
+
+    public String toString(){
+        return "\nNome: "+this.getNome()+"\nQuantidade de Funcionários: "+this.getQuantidadeFuncionario()+
+                "\nSalário Base dos Funcionários: "+this.getSalarioBaseFuncionario()+"\n"+
+                this.getEndereco().toString()+"\nData de Fundação: "+this.getDataDeFundacao().toString()+
+                "\nTaxa de Comercialização: R$"+this.getTaxaComercializacao();
+    }
+}
