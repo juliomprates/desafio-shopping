4a76c1c56554e9ce8033c75b90c266fc81732bf1
Júlio Prates <juliopfeijo@gmail.com>
2025-04-25 17:51:00 -0300

    feat(classes): criação da subclasse Vestuario, que é um tipo de Loja, com um atributo específico que informa se a loja possui produtos importados ou não

---
+++ subclassesLoja/Vestuario.java
@@ -0,0 +1,34 @@
+package subclassesLoja;
+
+import classesPrincipais.*;
+
+public class Vestuario extends Loja {
+    boolean produtosImportados = false;
+
+    public Vestuario(String _nome, int _qtdDeFuncionario, double _salarioBaseFuncionario,
+                     Endereco _endereco, Data _dataDeFundacao, boolean produtosImportados)
+    {
+        super(_nome, _qtdDeFuncionario, _salarioBaseFuncionario, _endereco, _dataDeFundacao);
+        this.produtosImportados = produtosImportados;
+    }
+
+    public Vestuario(String _nome, int _qtdDeFuncionario, Endereco _endereco,
+                     Data _dataDeFundacao, boolean produtosImportados)
+    {
+        super(_nome, _qtdDeFuncionario, _endereco, _dataDeFundacao);
+        this.produtosImportados = produtosImportados;
+    }
+
+    public String isProdutosImportados() {
+        if (produtosImportados == true) return "Sim";
+        return "Não";
+    }
+
+    public void setProdutosImportados(boolean produtosImportados) {
+        this.produtosImportados = produtosImportados;
+    }
+
+    public String toString(){
+        return super.toString()+"\nVende produtos Importados? "+isProdutosImportados();
+    }
+}
